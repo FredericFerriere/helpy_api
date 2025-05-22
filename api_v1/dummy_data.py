adcc97115e9c5b9a9507790c766fe53f5835894a
Frederic Ferriere <57955693+FredericFerriere@users.noreply.github.com>
2025-05-22 15:53:29 +0200

    abstraction layers

--- api_v1/dummy_data.py
+++ api_v1/dummy_data.py
@@ -5,8 +5,9 @@ from shapely.geometry import Point
 import geopandas
 
 from .models import User, Restaurant, UserRestaurantRating
-from .database_manager import DatabaseManager
+from .database_utility import add_record, create_db_and_tables
 from .constants import PROJECTED_MERCATOR_CRS, WORLD_GEODETIC_CRS
+from .session_manager import SessionManager
 
 
 def generate_random_point(latitude: float, longitude: float, max_radius: float):
@@ -47,32 +48,33 @@ def create_sample():
     restaurants = []
     cities = [[48.8620625, 2.3427284, 5000], [45.90751266479492, 6.124344348907471, 3000]]
 
-    for j in range(num_restaurants):
-        city_pick = cities[0 if random() < 0.8 else 1]
-        latitude = city_pick[0]
-        longitude = city_pick[1]
-        radius = city_pick[2]
-        lat, lon = generate_random_point(latitude, longitude, radius)
+    with SessionManager.create_session() as session:
+        for j in range(num_restaurants):
+            city_pick = cities[0 if random() < 0.8 else 1]
+            latitude = city_pick[0]
+            longitude = city_pick[1]
+            radius = city_pick[2]
+            lat, lon = generate_random_point(latitude, longitude, radius)
 
-        restaurant = Restaurant(name='restaurant_{}'.format(j), coordinates='POINT({} {})'.format(lon, lat))
-        DatabaseManager.add_record(restaurant)
-        restaurants.append(restaurant)
+            restaurant = Restaurant(name='restaurant_{}'.format(j), coordinates='POINT({} {})'.format(lon, lat))
+            add_record(session, restaurant)
+            restaurants.append(restaurant)
 
-    for i in range(num_users):
-        user = User(user_alias='dummy_{}'.format(i))
-        DatabaseManager.add_record(user)
-        users.append(user)
+        for i in range(num_users):
+            user = User(user_alias='dummy_{}'.format(i))
+            add_record(session, user)
+            users.append(user)
 
-        num_eval = randint(min_eval, max_eval)
-        rest_ids = sample(range(num_restaurants), num_eval)
-        for el in rest_ids:
-            restaurant = restaurants[el]
-            rating = UserRestaurantRating(user_id=user.id, restaurant_id=restaurant.id, rating=randint(1, 10))
-            DatabaseManager.add_record(rating)
+            num_eval = randint(min_eval, max_eval)
+            rest_ids = sample(range(num_restaurants), num_eval)
+            for el in rest_ids:
+                restaurant = restaurants[el]
+                rating = UserRestaurantRating(user_id=user.id, restaurant_id=restaurant.id, rating=randint(1, 10))
+                add_record(session, rating)
 
 
 def main():
-    DatabaseManager.create_db_and_tables()
+    create_db_and_tables()
     create_sample()
     return
 
--- api_v1/main.py
+++ api_v1/main.py
@@ -1,11 +1,22 @@
 import logging
+import uuid
+from datetime import datetime
 
-from .app_manager import AppManager
+from fastapi import Query, HTTPException
+from sqlmodel import select, func
+from geoalchemy2.shape import to_shape
+from geoalchemy2.functions import ST_GeogFromText, ST_Distance
+
+from .app_utility import create_app
+from .models import User, Restaurant, UserRestaurantRating
+from .session_manager import SessionManager
+from .database_utility import add_record
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-app = AppManager.create_app()
+app = create_app()
 
 
 @app.get('/')
@@ -14,15 +25,55 @@ def get_root():
 
 
 @app.get('/users/{user_id}/restaurants/my_notations')
-async def get_known_restaurants_user(user_id):
-    return AppManager.get_known_restaurants_user(user_id)
+async def get_known_restaurants_user(user_id: uuid.UUID):
+    with SessionManager.create_session() as session:
+        statement = select(Restaurant, UserRestaurantRating).join(UserRestaurantRating).where(
+            UserRestaurantRating.user_id == user_id)
+        results = session.exec(statement)
+        res = []
+        for restaurant, rating in results:
+            res.append({'name': restaurant.name,
+                        'notation': rating.rating,
+                        'user_id': rating.user_id,
+                        'location': to_shape(restaurant.coordinates).wkt})
+        return res
 
 
 @app.get('/users/{user_id}/restaurants/geographic_filter/')
-async def get_restaurant_suggestions_user(user_id, latitude, longitude, radius):
-    return AppManager.get_restaurant_suggestions_user(user_id, latitude, longitude, radius)
+async def get_restaurant_suggestions_user(user_id: uuid.UUID, latitude: float = Query(ge=-90, le=90),
+                                        longitude: float = Query(ge=-180, le=180), radius: int = Query(ge=1, le=10000)):
+    with SessionManager.create_session() as session:
+        statement = select(
+            Restaurant.id,
+            Restaurant.name,
+            Restaurant.coordinates,
+            func.avg(UserRestaurantRating.rating).label('average_rating')
+        ).join(UserRestaurantRating).where(
+            ST_Distance(Restaurant.coordinates, ST_GeogFromText('SRID=4326;POINT({} {})'
+                                                                .format(longitude, latitude))) < radius
+        ).group_by(Restaurant.id)
+        results = session.exec(statement)
+        res = [{'id': restaurant_id,
+                'name': restaurant_name,
+                'location': to_shape(location).wkt,
+                'average_rating': round(rating, 1)}
+               for restaurant_id, restaurant_name, location, rating in results]
+        return res
 
 
 @app.post('/users/{user_id}/restaurants/new_rating/')
-async def put_restaurant_rating(user_id, restaurant_id, rating, rating_date, visit_date):
-    return AppManager.put_restaurant_rating(user_id, restaurant_id, rating, rating_date, visit_date)
+async def add_restaurant_rating(user_id: uuid.UUID, restaurant_id: uuid.UUID, rating: int = Query(ge=1, le=10),
+                              rating_date: datetime = datetime.now(), visit_date: datetime | None = None):
+    with SessionManager.create_session() as session:
+        user = session.get(User, user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        restaurant = session.get(Restaurant, restaurant_id)
+        if not restaurant:
+            raise HTTPException(status_code=404, detail="Restaurant not found")
+
+        new_rating = UserRestaurantRating(user_id=user_id, restaurant_id=restaurant_id, rating=rating,
+                                          rating_date=rating_date, visit_date=visit_date)
+        add_record(session, new_rating)
+        return new_rating
